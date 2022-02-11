c04404edb42831555dd95d66394c80eb9038871d
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-02-11 17:34:08 +0300

    Android View and permission

--- app/src/main/java/com/mwalagho/ferdinand/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/mwalagho/ferdinand/qrcodescanner/MainActivity.kt
@@ -21,8 +21,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import com.mwalagho.ferdinand.qrcodescanner.ui.theme.QRCodeScannerTheme
 import java.lang.Exception
@@ -32,17 +37,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             QRCodeScannerTheme {
-                var code by remember{
+                var code by remember {
                     mutableStateOf("")
 
                 }
 
                 val context = LocalContext.current
-                val cameraProviderFuture = remember{
+                val lifecycleOwner = LocalLifecycleOwner.current
+                val cameraProviderFuture = remember {
                     ProcessCameraProvider.getInstance(context)
                 }
 
-                var hasCamPermission by remember{
+                var hasCamPermission by remember {
                     mutableStateOf(
                         ContextCompat.checkSelfPermission(
                             context,
@@ -53,38 +59,53 @@ class MainActivity : ComponentActivity() {
 
                 val launcher = rememberLauncherForActivityResult(
                     contract = ActivityResultContracts.RequestPermission(),
-                    onResult = {granted -> hasCamPermission = granted
+                    onResult = { granted ->
+                        hasCamPermission = granted
                     }
                 )
-                LaunchedEffect(key1 = true){
+                LaunchedEffect(key1 = true) {
                     launcher.launch(Manifest.permission.CAMERA)
                 }
 
                 Column(modifier = Modifier.fillMaxSize()) {
-                    AndroidView(factory = { context ->
-                        val previewView = PreviewView(context)
-                        val preview = Preview.Builder().build()
-                        val selector = CameraSelector.Builder()
-                            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                            .build()
-                        preview.setSurfaceProvider(previewView.surfaceProvider)
-                        val imageAnalysis = ImageAnalysis.Builder()
-                            .setTargetResolution(Size(previewView.width, previewView.height))
-                            .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)//method to define what happens if camera renders faster than code can abalyze
-                            .build()
+                    if (hasCamPermission) {
+                        AndroidView(factory = { context ->
+                            val previewView = PreviewView(context)
+                            val preview = Preview.Builder().build()
+                            val selector = CameraSelector.Builder()
+                                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                                .build()
+                            preview.setSurfaceProvider(previewView.surfaceProvider)
+                            val imageAnalysis = ImageAnalysis.Builder()
+                                .setTargetResolution(Size(previewView.width, previewView.height))
+                                .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)//method to define what happens if camera renders faster than code can abalyze
+                                .build()
 
-                        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(context), QRCodeAnalyzer{
-                            result -> code = result
-                        }
+                            imageAnalysis.setAnalyzer(
+                                ContextCompat.getMainExecutor(context),
+                                QRCodeAnalyzer { result ->
+                                    code = result
+                                }
+                            )
+                            try {
+                                cameraProviderFuture.get().bindToLifecycle(
+                                    lifecycleOwner, selector, preview, imageAnalysis
+                                )
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                            }
+                            previewView
+
+                        })
+                        Text(
+                            text = code,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(32.dp)
                         )
-                        try{
-
-                        } catch (e:Exception){
-                            e.printStackTrace()
-                        }
-
-                    })
-
+                    }
                 }
             }
         }
