bf11adc6159d2ba3a7391650f8d24cae4e2844f4
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-02-11 17:29:02 +0300

    Camera permissions

--- app/src/main/java/com/mwalagho/ferdinand/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/mwalagho/ferdinand/qrcodescanner/MainActivity.kt
@@ -1,38 +1,94 @@
 package com.mwalagho.ferdinand.qrcodescanner
 
 import android.os.Bundle
+import android.util.Size
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.mwalagho.ferdinand.qrcodescanner.ui.theme.QRCodeScannerTheme
+import java.lang.Exception
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             QRCodeScannerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                var code by remember{
+                    mutableStateOf("")
+
+                }
+
+                val context = LocalContext.current
+                val cameraProviderFuture = remember{
+                    ProcessCameraProvider.getInstance(context)
+                }
+
+                var hasCamPermission by remember{
+                    mutableStateOf(
+                        ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.CAMERA
+                        ) == PackageManager.PERMISSION_GRANTED
+                    )
+                }
+
+                val launcher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = {granted -> hasCamPermission = granted
+                    }
+                )
+                LaunchedEffect(key1 = true){
+                    launcher.launch(Manifest.permission.CAMERA)
+                }
+
+                Column(modifier = Modifier.fillMaxSize()) {
+                    AndroidView(factory = { context ->
+                        val previewView = PreviewView(context)
+                        val preview = Preview.Builder().build()
+                        val selector = CameraSelector.Builder()
+                            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                            .build()
+                        preview.setSurfaceProvider(previewView.surfaceProvider)
+                        val imageAnalysis = ImageAnalysis.Builder()
+                            .setTargetResolution(Size(previewView.width, previewView.height))
+                            .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)//method to define what happens if camera renders faster than code can abalyze
+                            .build()
+
+                        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(context), QRCodeAnalyzer{
+                            result -> code = result
+                        }
+                        )
+                        try{
+
+                        } catch (e:Exception){
+                            e.printStackTrace()
+                        }
+
+                    })
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    QRCodeScannerTheme {
-        Greeting("Android")
-    }
-}
